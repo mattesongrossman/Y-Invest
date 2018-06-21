63b72aa2e7b0134349f64e174b68df24519758a9
Matteson Grossman <mattesongrossman@Mattesons-MacBook-Pro.local>
2018-06-21 16:41:33 -0400

    index change

--- src/index.js
+++ src/index.js
@@ -19,10 +19,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 )
-
-// import React from "react"
-// import ReactDOM from "react-dom"
-// import "assets/scss/material-dashboard-pro-react.css?v=1.1.0"
-// import App from "./App"
-//
-// ReactDOM.render(<App />, document.getElementById("root"))
